aec112aeaf0660fc649d58d9dfad49ba79b6523e
Kevin Harvey <harveykp@hotmail.com>
2019-06-24 14:25:06 -0500

    syncing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import "../scss/styles.scss";
 
 import Collapse from "bootstrap/js/src/collapse";
+import $ from "jquery";
+
+$(".sidebar .close").click(function(){
+    $(".sidebar").addClass("hide");
+})
