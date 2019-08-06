8bb6c66ce5e4867dfc9bb3f23179c24d17604700
Jonathan Siler <jonathan@volanosolutions.com>
2019-08-06 12:04:33 -0500

    Add kinematics to finder rotation.

--- src/js/modules/home/finder.js
+++ src/js/modules/home/finder.js
@@ -1,18 +1,20 @@
 (function(w){
-    let finder = document.getElementById("finder"),
+    let throttle,
+        finder = document.getElementById("finder"),
         medallion = finder.querySelector(".medallion"),
-        throttle,
-        transition = ((e) => {
+        options = finder.querySelector(".finder-nav"),
+        navigation = finder.querySelectorAll(".finder-nav button[class*=rotate]"),
+        transition = (e) => {
             if(throttle){
                 w.clearTimeout(throttle);
             }
-    
+
             throttle =  w.setTimeout(function(){
                 let beginMove = 320,
                     endMove = 200,
                     offset = finder.getBoundingClientRect().top,
                     percentComplete = Math.min(100, Math.max(0, (beginMove - offset)/(beginMove - endMove) * 100));
-    
+
                 if(finder && finder.clientHeight > 0) {
                     medallion.style.top = `${percentComplete/2}%`;
                     finder.querySelector(".left-nav").style.left = `${percentComplete/2}%`;
@@ -20,10 +22,27 @@
                 } else {
                     finder.querySelectorAll(".medallion, .left-nav, .right-nav").map((e)=> e.style = null);
                 }
-    
+
             }, 10);
-        })();
+        };
 
     w.addEventListener('scroll', transition);
+    w.addEventListener('load', transition);
+    if(navigation){
+        [...navigation].map((e)=> e.addEventListener('click', rotateOptions));
+    }
+
+    /**
+     * Rotate the options around in the option lists for symettry
+     */
+    function rotateOptions (element){
+        let leftNav = options.querySelector(".left-nav"),
+            rightNav = options.querySelector(".right-nav"),
+            direction = element.currentTarget.classList.contains("rotate-cw")?"clockwise":"counterclockwise",
+            kinematic = direction === "clockwise" ? [leftNav, rightNav] : [rightNav, leftNav];
+        kinematic[0].append(kinematic[0].firstElementChild);
+        kinematic[1].prepend(kinematic[1].lastElementChild);
+        return false;
+    }
 
 })(window);
